746e09d82e4c531a29577ff005bc422286e473e8
wbryon <hatami1974@yandex.ru>
2022-10-06 22:25:12 +0300

    Удалил неиспользуемые переменную result и метод setStepLength в классе Converter

--- src/Converter.java
+++ src/Converter.java
@@ -5,20 +5,12 @@ import java.math.RoundingMode;
  * В этом классе осуществляется преобразование шагов в километры и калории
  */
 public class Converter {
-    BigDecimal result;
 
     private int stepLength = 75;
     private final double calories = 50;
     private final double kilometer = 100000;
     private final double kilocaloriie = 1000;
-    /** Метод для изменения длины шага (создан как дополнительная опция к программе) */
-    public void setStepLength(int stepLength) {
-        if (stepLength >= 0) {
-            this.stepLength = stepLength;
-        } else {
-            System.out.println("\nРазмер шага не может быть отрицательным, установлено значение по умолчанию!");
-        }
-    }
+
     /** Метод для преобразования количества пройденных шагов в километры */
     void convertStepsToKilometers(int stepCount) {
         System.out.println("Пройденное расстояние: " + new BigDecimal(stepCount * stepLength / kilometer).setScale(2, RoundingMode.DOWN) + " км");
